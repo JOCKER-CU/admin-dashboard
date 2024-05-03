75784ff7fc3034862a836a638fe6ee01844ed0b7
JOCKER-CU <45041524+JOCKER-CU@users.noreply.github.com>
2024-05-03 14:22:24 +0630

    admin dashboard tutorial testing
    Admin dashboard making with html, css, js

---
+++ admin-dashbaord/orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Fodable Mini Done',
+        productNumber: '838392',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '23043',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Done',
+        productNumber: '234003',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '29348',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Drone with Camera Done',
+        productNumber: '222344',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
